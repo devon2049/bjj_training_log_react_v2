f9ea5f73acc441634cd844478021200b57942be8
devon2049 <dclgoog@gmail.com>
2024-08-23 11:02:13 -0400

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+// import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Logout from './components/Logout';
